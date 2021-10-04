370c50fb700b3843f97562cd440ead04eb1580fe
vishwajeet51 <91774671+vishwajeet51@users.noreply.github.com>
2021-10-04 10:26:09 +0530

    Create Min_in_array.cpp

---
+++ C++/Basic questions/Min_in_array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+   int n, i, minimum, a[10] = {1, 6, 7, 10, 12, 14, 12, 16, 20, 26};
+   char ch;
+   minimum = a[0];
+   cout<<"\nThe data element of array:";
+   for(i = 0; i < 10; i++) {
+      cout<<" "<<a[i];
+      if(minimum > a[i])
+         minimum= a[i];
+   }
+   cout<<"\n\nMinimum of the data elements of array using linear search is: "<<minimum;
+   return 0;
+}
